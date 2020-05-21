a4c36e4d37cc534bcacbb948964d8baceafca25d
BrionStoutarm <macksmith675@gmail.com>
2020-05-21 15:46:14 -0700

    Left and right movement
    Basic movement left and right
    
    Todo:  acceleration and gravity

---
+++ rope-swing/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+    }
+
+    void GetInput()
+    {
+        Vector2 pos = transform.position;
+
+        //move left
+        if(Input.GetKeyDown(KeyCode.A) || Input.GetKey(KeyCode.A))
+        {
+            pos.x -= GameConstants.movement_delta;
+        }
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKey(KeyCode.D))
+        {
+            pos.x += GameConstants.movement_delta;
+        }
+
+        transform.position = Vector2.Lerp(transform.position, pos, 1.0f * Time.fixedDeltaTime);
+    }
+}
---
+++ rope-swing/Assets/Scripts/World/GameConstants.cs
@@ -0,0 +1,8 @@
+﻿
+public static class GameConstants 
+{
+    public const int movement_delta = 1;
+    public const double max_velocity = 1.0;
+
+    public const float time_step = 0;
+}
